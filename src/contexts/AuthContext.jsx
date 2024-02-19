f12215e6784d1e3d5731938d0df5fed810f96e02
nhnahid1122 <mdxx0356@stud.kea.dk>
2024-02-19 12:49:21 +0100

    impement firbase authentication

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,74 @@
+import React, { useContext, useEffect, useState, useMemo } from "react";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+
+// internal import
+import "../../firbase.js";
+
+const AuthContext = React.createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export default function AuthProvider({ children }) {
+  const [loading, setLoading] = useState(true);
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setLoading(false);
+
+      return () => unsubscribe();
+    });
+  }, []);
+
+  // signup function
+  const signup = async (email, password, username) => {
+    try {
+      const auth = getAuth();
+      await createUserWithEmailAndPassword(auth, email, password);
+      await updateProfile(auth.currentUser, { displayName: username });
+      setCurrentUser(auth.currentUser);
+    } catch (error) {
+      console.error("Error signing up:", error);
+      throw error;
+    }
+  };
+
+  // login function
+  const login = (email, password) => {
+    const auth = getAuth();
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  // logout function
+  const logout = () => {
+    const auth = getAuth();
+    return signOut(auth);
+  };
+
+  const value = useMemo(
+    () => ({
+      currentUser,
+      signup,
+      login,
+      logout,
+    }),
+    [currentUser]
+  );
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+}
